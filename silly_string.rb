275391238278fe4a636dc20903ebc76abdc900d9
debbie <4debgonzalez@gmail.com>
2017-06-06 15:24:28 +0000

    finished silly string lab

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,13 @@
 #code your solutions here
+my_word = "kwk"
+puts my_word.upcase
+shout = "I'M NOT SHOUTING"
+puts shout.downcase
+name = "Karlie Kloss"
+puts name.length
+puts name + " wow" "mom"
+sentence = "i really like programming"
+puts sentence.capitalize
 
 
 
